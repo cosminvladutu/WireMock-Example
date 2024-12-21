e49518135c30990b6482357f6076c521c273c646
Cosmin Vladutu <cosmin.vladutu@arnia.ro>
2024-12-21 13:18:46 +0200

    Add orchestration.

---
+++ WeatherFunction/WeatherOrchestrator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+
+[DurableTask]
+public class WeatherOrchestrator
+{
+    [Function(nameof(WeatherOrchestrator))]
+    public async Task<string> RunOrchestrator([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        string city = context.GetInput<string>();
+
+        var coordinates = await context.CallActivityAsync<(double lat, double lon)>(nameof(GetCoordinates), city);
+        var weatherData = await context.CallActivityAsync<string>(nameof(GetWeatherData), coordinates);
+
+        return weatherData;
+    }
+}
+
+
+
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.DurableTask;
+
+//[DurableTask]
+//public class WeatherOrchestrator
+//{
+//    [Function(nameof(WeatherOrchestrator))]
+//    public async Task<string> RunAsync(
+//               [OrchestrationTrigger] TaskOrchestrationContext context)
+//    {
+//        var city = context.GetInput<string>();
+
+
+//        var coordinates = await context.CallActivityAsync<(double lat, double lon)>(nameof(GetCoordinates), city);
+//        var weatherData = await context.CallActivityAsync<string>(nameof(GetWeatherData), coordinates);
+
+//        return await Task.FromResult(city);
+//    }
+//}
